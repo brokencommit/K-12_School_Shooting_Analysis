ab244d5221d2eba21085fbfa700656113efe5868
jatr6994 <JacobTran1996@gmail.com>
2020-04-15 00:39:12 -0600

    read me image

--- README.md
+++ README.md
@@ -7,3 +7,5 @@
 - Jupyter Notebook when Python 3.8 is supported
 - pretty plots
 ```
+
+![Image description](https://github.com/brokencommit/K-12_School_Shooting_Analysis/blob/master/2000-2020.png)
--- school_shooting_data.py
+++ school_shooting_data.py
@@ -12,20 +12,19 @@ df = pd.read_csv( file, escapechar='\\', skiprows=[0] )
 df['DateTime'] = pd.to_datetime(df['Date'], format='%m/%d/%Y', errors='coerce' )
 
 totalDeaths = df.groupby(['DateTime'])['Total Injured/Killed Victims'].sum().reset_index(level=0)
-mask = (totalDeaths['DateTime'] >= '2009-01') & (totalDeaths['DateTime'] <= '2020-03')
+mask = (totalDeaths['DateTime'] >= '2000-01') & (totalDeaths['DateTime'] <= '2020-03')
 totalDeaths = totalDeaths.loc[mask]
 
-ax = totalDeaths.plot( x='DateTime', y='Total Injured/Killed Victims' )
+ax = totalDeaths.plot( x='DateTime', y='Total Injured/Killed Victims', color='steelblue', linewidth=2 )
 
 ax.xaxis.set_major_locator(mdates.YearLocator())
 ax.xaxis.set_major_formatter(mdates.DateFormatter('\n%Y'))
 ax.xaxis.set_minor_formatter(mdates.DateFormatter('%m'))
 ax.tick_params( axis='both', which='major', labelsize=15 )
 plt.setp( ax.get_xticklabels(), rotation=45, ha='center' )
-ax.set_ylabel( 'Total Injured/Killed Victims', size=30 )
 
 ax.set_title( 'Shootings On School Property', fontsize=40 )
-ax.set_xlabel( 'DateTime', size=30 )
+ax.set_xlabel( 'Year', size=30 )
 ax.set_ylabel( 'Total Injured/Killed Victims', size=30 )
 
 plt.show()
